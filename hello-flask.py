4e4b15600c625d9f275b59d53ea3dc5f36e6d98a
maxtkc <maxkatzchristy@gmail.com>
2018-04-12 23:32:17 -0400

    modified so that it relays http get to serial

--- hello-flask.py
+++ hello-flask.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import serial
 app = Flask(__name__)
+ser = serial.Serial(4)
 
-@app.route("/")
-def hello():
-	return "Hello World!"
+@app.route("/<str>")
+def hello(str):
+    ser.write(str) # send the command
+    return myArduinoCommand + " " + str(ser.outWaiting()) + " " + str(ser.inWaiting())
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, debug=True)
